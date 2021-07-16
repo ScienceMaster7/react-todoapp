fdc9f151bdc9d95cc68d51ec56b8754e3c68507f
Benedikt Walch <benx.bw@gmail.com>
2021-07-16 11:17:12 +0200

    Create header with logo input field and submit button

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [todos, setTodos] = useState([]);
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <header className="header">
+        <img className="logo" src="./logo192.png" alt="react logo" />
+        <form>
+          <input
+            className="addTodoInput"
+            type="text"
+            name="todoInput"
+            id="todoInput"
+            placeholder="What needs to be done ?"
+            autoComplete="off"
+          />
+          <button className="addTodoButton">Add</button>
+        </form>
+      </header>
     </div>
   );
 }
