d847c9299cae3f0525dc0cd11b6c1477374f8fe1
Benedikt Walch <benx.bw@gmail.com>
2021-07-16 15:59:45 +0200

    Adition of Check icon, and styling

--- src/components/CreateTodos.js
+++ src/components/CreateTodos.js
@@ -1,4 +1,6 @@
 import "./CreateTodos.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCheck } from "@fortawesome/free-solid-svg-icons";
 
 export default function CreateTodos({ todo, deletetask }) {
   function Delete() {
@@ -8,6 +10,9 @@ export default function CreateTodos({ todo, deletetask }) {
     <section className="todoContainer">
       <button onClick={Delete} className="todo__DeleteButton">
         X
+      </button>
+      <button className="todo__DoneButton">
+        <FontAwesomeIcon icon={faCheck} />
       </button>
       <li className="todo__listitem">{todo.task}</li>
       <p className="todo__text">status</p>
